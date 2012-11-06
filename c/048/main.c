99b865cc34920fea20f69bad3467d3c89619a963
yizha <ding.rickcat@gmail.com>
2012-11-06 22:28:29 +0800

    add another solution to p48

--- c/048/main.c
+++ c/048/main.c
@@ -29,8 +29,19 @@ void *add(int *a, int *b) {
     }
 }
 
+void simple_and_fast() {
+    unsigned long long sum = 0;
+    for(int i = 1; i <= 1000; i++) {
+        unsigned long long n = i;
+        for(int j = 1; j < i; j++)
+            n = (i * n) % 10000000000L;
+        sum += n;
+    }
+    printf("%llu\n", sum % 10000000000L);
+}
+
 int main(int argc, char* argv) {
-    int sum[] = {0,0,0,0,0,0,0,0,0,0};
+    /*int sum[] = {0,0,0,0,0,0,0,0,0,0};
     int p[] = {0,0,0,0,0,0,0,0,0,0};
     for (int i = 1; i < 1001; i++) {
         if ((i % 10) == 0) continue;
@@ -41,5 +52,7 @@ int main(int argc, char* argv) {
     for (int i = 9; i >= 0; i--) {
         printf("%d", sum[i]);
     }
-    printf("\n");
+    printf("\n");*/
+
+    simple_and_fast();
 }
