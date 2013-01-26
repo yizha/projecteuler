20671ee7048825d993b369b05587d227025b31ac
yizha <ding.rickcat@gmail.com>
2013-01-26 16:51:00 +0800

    more

--- c/078/main.c
+++ c/078/main.c
@@ -22,7 +22,50 @@ void solve() {
     }
 }
 
+void fast() {
+    int partition[TOP];
+
+    for (int i = 1; i < TOP; i++) partition[i] = 0;
+    partition[0] = 1;
+
+    for (int i = 1; i < TOP; i++) {
+        int part_n = 0;
+        int pentagon = 0;
+        int k = 1;
+        while (1) {
+            pentagon = pentagon + ((k<<1) - 1);
+            if (pentagon > i) break;
+            int x = k & 1;
+            if (x == 1) {
+                part_n = part_n + partition[i - pentagon];
+            } else {
+                part_n = part_n + DIV - partition[i - pentagon];
+            }
+            if (part_n > DIV) part_n = part_n - DIV;
+
+            pentagon = pentagon + k;
+            if (pentagon > i) break;
+            if (x == 1) {
+                part_n = part_n + partition[i - pentagon];
+            } else {
+                part_n = part_n + DIV - partition[i - pentagon];
+            }
+            if (part_n > DIV) part_n = part_n - DIV;
+
+            k++;
+        }
+        //if (part_n > DIV) part_n = part_n - DIV;
+        partition[i] = part_n;
+        //printf("%d --> %d\n", i, part_n);
+        if (part_n == DIV) {
+            printf("%d --> %d\n", i, part_n);
+            break;
+        }
+    }
+}
+
 int main(int argc, char* argv) {
-    solve();
+    //solve();
+    fast();
     return 0;
 }
